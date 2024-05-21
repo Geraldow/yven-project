542fb0f4518b6614ccb8d67d01b079b8e8ad43b2
76287712 <76287712@certus.edu.pe>
2024-05-21 16:15:21 -0500

    Se terminá Sección Categoria

--- src/js/slider-ventajas.js
+++ src/js/slider-ventajas.js
@@ -26,3 +26,32 @@ let swiperCards = new Swiper('.card-content', {
     },
   
   });
+
+
+// Función Ventajas
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Mostrar la pestaña "Todos los eventos" por defecto
+  openTab('todos', {
+    currentTarget: document.querySelector('.tablink')
+  });
+});
+
+function openTab(tabName, event) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].classList.remove("active");
+  }
+  document.getElementById(tabName).style.display = "block";
+  if (event) {
+    event.currentTarget.classList.add("active");
+    var underline = document.getElementById("underline");
+    underline.style.width = event.currentTarget.offsetWidth + "px";
+    underline.style.left = event.currentTarget.offsetLeft + "px";
+  }
+}
